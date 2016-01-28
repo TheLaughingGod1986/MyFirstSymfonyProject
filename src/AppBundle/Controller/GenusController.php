df9256c94b339ccf82fafd5faec3148cfd9fe4c0
TheLaughingGod1986 <benoats@gmail.com>
2016-01-28 22:38:39 +0000

    symfony gettin hard

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -19,10 +19,15 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        $templating = $this->container->get('templating');
-        $html = $templating->render('genus/show.html.twig' , [
-        'name' => $genusName
+       $notes = [
+        'octopus asked me a riddle, outsmarted me',
+        'i counted 8 legs... as they wraped around me',
+        'inked!'
+        ];
+
+        return $this->render('genus/show.html.twig' , [
+        'name' => $genusName,
+            'notes' => $notes
         ]);
-        Return new Response($html);
     }
 }
